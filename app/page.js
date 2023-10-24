bf63cf2a254428c9366977fbb1cf2554197dc694
Muhammad Faizan <mohammadfaizan98x@gmail.com>
2023-10-24 23:33:54 +0500

    first cmnt

--- app/page.js
+++ app/page.js
@@ -1,95 +1,57 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
+
+import AddIcon from "@mui/icons-material/Add";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import SavingsIcon from "@mui/icons-material/Savings";
+import PaidIcon from "@mui/icons-material/Paid";
+import AdjustIcon from "@mui/icons-material/Adjust";
+import ShowChartOutlinedIcon from "@mui/icons-material/ShowChartOutlined";
+import SignalCellularAltSharpIcon from "@mui/icons-material/SignalCellularAltSharp";
+import FolderCopyOutlinedIcon from "@mui/icons-material/FolderCopyOutlined";
+import CalculateOutlinedIcon from "@mui/icons-material/CalculateOutlined";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className={styles.screen}>
+      <header className={styles.header}>
+        <div className={styles.bars}>
+          <MenuIcon />
         </div>
-      </div>
+        <div className={styles.title}>
+          <h3 className={styles.titleName}>Accounts</h3>
+        </div>
+        <div className={styles.add}>
+          <AddIcon />
+        </div>
+      </header>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className={styles.main}>
+        <div className={styles.box1}>
+          <AccountBalanceWalletIcon className={styles.icon} />
+          <p className={styles.iconHeading}>MyAccounts</p>
+        </div>
+        <div className={styles.box2}>
+          <SavingsIcon className={styles.icon} />
+          <p className={styles.iconHeading}>MySavings</p>
+        </div>
+        <div className={styles.box3}>
+          <AdjustIcon className={styles.icon} />
+          <p className={styles.iconHeading}>MyGoals</p>
+        </div>
+        <div className={styles.box4}>
+          <PaidIcon className={styles.icon} />
+          <p className={styles.iconHeading}>MyDebts</p>
+        </div>
+      </main>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer className={styles.footer}>
+        <ShowChartOutlinedIcon />
+        <FolderCopyOutlinedIcon className={styles.icon2} />
+        <AddIcon className={styles.plusbtn} />
+        <CalculateOutlinedIcon />
+        <SignalCellularAltSharpIcon />
+      </footer>
+    </section>
+  );
 }
